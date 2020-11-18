2c162e35b57b4b7c962b126fec07d47ef9c0a9c5
Kate Mihailovskaia <lesa182011@gmail.com>
2020-11-18 08:15:54 +0300

    all file add

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-menu.onclick = function(event) {
-    var menu = document.getElementById('menu-mobile');
-    toggle(menu);
-  }
-  function toggle(el) {
-    el.style.display = (el.style.display == 'none') ? 'block' : 'none'
-  }
-  
